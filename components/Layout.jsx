c02f935b08646dc6c5bd05c873cb38ea67e5326a
Ivan Grigor <vanya.grigorevskiy@gmail.com>
2023-10-11 19:18:07 +0300

    add dev

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,7 +4,6 @@ module.exports = function Layout({ title, children }) {
     <html lang="en">
       <head>
         <title>{title}</title>
-        <div></div>
         <link rel="stylesheet" href="/styles/style.css" />
         <script defer src="/scripts/auth.js" />
       </head>
