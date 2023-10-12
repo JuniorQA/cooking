918eeb36adacd8935ed2ff2bffc66b46683558ab
Ivan Grigor <vanya.grigorevskiy@gmail.com>
2023-10-12 19:32:29 +0300

    hot fix after MC

--- public/scripts/sortRecipe.js
+++ public/scripts/sortRecipe.js
@@ -1,13 +1,8 @@
-
-
-
-
-
 function sortArray(array, flag) {
-    if (flag === 'sak') {
-   array.sort((a ,b) => a.cooking_time - b.cooking_time )
-    } else if (flag === "dac") {
-        array.sort((a ,b) => b.cooking_time - a.cooking_time )
-    }
+  if (flag === 'asc') {
+    array.sort((a, b) => a.cooking_time - b.cooking_time);
+  } else if (flag === 'desc') {
+    array.sort((a, b) => b.cooking_time - a.cooking_time);
+  }
 }
-  module.exports = sortArray
+module.exports = sortArray;
--- routes/view/main.routes.js
+++ routes/view/main.routes.js
@@ -1,33 +1,29 @@
-const router = require('express').Router()
-const Main = require('../../components/Main')
-const RecipePage = require('../../components/RecipePage')
-const { Recipe } = require('../../db/models')
-const shuffleArray = require('../../public/scripts/shuffle')
-const sotrRecipe = require('../../public/scripts/sortRecipe')
+const router = require('express').Router();
+const Main = require('../../components/Main');
+const RecipePage = require('../../components/RecipePage');
+const { Recipe } = require('../../db/models');
+const shuffleArray = require('../../public/scripts/shuffle');
+const sotrRecipe = require('../../public/scripts/sortRecipe');
 
 router.get('/', async (req, res) => {
   const recipes = await Recipe.findAll();
-  sotrRecipe(recipes)
+  shuffleArray(recipes);
   const html = res.renderComponent(Main, { title: 'cooking-book', recipes });
   res.send(html);
 });
-	const recipes = await Recipe.findAll()
-	shuffleArray(recipes)
-	const html = res.renderComponent(Main, { title: 'cooking-book', recipes })
-	res.send(html)
-})
 
 router.get('/recipeId', async (req, res) => {
-	try {
-		const { recipeId } = req.params
-		const recipe = await Recipe.findOne({ where: { id: recipeId } })
-		const html = res.renderComponent(RecipePage, {
-			title: 'Recipe page', recipe,
-		})
-		res.send(html)
-	} catch ({ message }) {
-		res.json({ message })
-	}
-})
+  try {
+    const { recipeId } = req.params;
+    const recipe = await Recipe.findOne({ where: { id: recipeId } });
+    const html = res.renderComponent(RecipePage, {
+      title: 'Recipe page',
+      recipe,
+    });
+    res.send(html);
+  } catch ({ message }) {
+    res.json({ message });
+  }
+});
 
-module.exports = router
+module.exports = router;
