ada2fd4a8072894aec1f52494573658a8c15266d
L1veng <i.gerasevv@gmail.com>
2023-10-12 19:08:30 +0300

    pagedown

--- components/RecipeItem.jsx
+++ components/RecipeItem.jsx
@@ -4,6 +4,9 @@ module.exports = function NavBar({ user, recipe }) {
   return (
     <div className="recipe-item-container flex flex-dir-col">
       <h2>Название: {recipe.name}</h2>
+      <a href={`/${recipe.id}`} className='btn btn-info btn-sm'>
+					Info
+				</a>
     </div>
   );
 };
--- routes/view/main.routes.js
+++ routes/view/main.routes.js
@@ -1,13 +1,27 @@
-const router = require('express').Router();
-const Main = require('../../components/Main');
-const { Recipe } = require('../../db/models');
+const router = require('express').Router()
+const Main = require('../../components/Main')
+const RecipePage = require('../../components/RecipePage')
+const { Recipe } = require('../../db/models')
 const shuffleArray = require('../../public/scripts/shuffle')
 
 router.get('/', async (req, res) => {
-  const recipes = await Recipe.findAll();
-  shuffleArray(recipes)
-  const html = res.renderComponent(Main, { title: 'cooking-book', recipes });
-  res.send(html);
-});
+	const recipes = await Recipe.findAll()
+	shuffleArray(recipes)
+	const html = res.renderComponent(Main, { title: 'cooking-book', recipes })
+	res.send(html)
+})
 
-module.exports = router;
+router.get('/recipeId', async (req, res) => {
+	try {
+		const { recipeId } = req.params
+		const recipe = await Recipe.findOne({ where: { id: recipeId } })
+		const html = res.renderComponent(RecipePage, {
+			title: 'Recipe page', recipe,
+		})
+		res.send(html)
+	} catch ({ message }) {
+		res.json({ message })
+	}
+})
+
+module.exports = router
