f4cc6c5fe37b92c67a1e201b940aab455fb51dd0
Ivan Grigor <vanya.grigorevskiy@gmail.com>
2023-10-13 16:25:48 +0300

    ref

--- components/RecipeItem.jsx
+++ components/RecipeItem.jsx
@@ -6,15 +6,17 @@ module.exports = function RecipeItem({ user, recipe, isFavorite }) {
       className="recipe-item-container flex flex-dir-col"
       data-id={recipe.id}
     >
-      <h2>{recipe.name}</h2>
+      <div className="flex btn-like-container">
+        <h2>{recipe.name}</h2>
+        {!isFavorite ? (
+          <button className="btn-favorite">❤︎</button>
+        ) : (
+          <button className="btn-remove-favorite">❤︎</button>
+        )}
+      </div>
       <a href={'/recipes/' + recipe.id}>
         <img src={recipe.image} alt={recipe.name} />
       </a>
-      {!isFavorite ? (
-        <button className="btn-favorite">❤︎</button>
-      ) : (
-        <button className="btn-remove-favorite">❤︎</button>
-      )}
     </div>
   );
 };
