190563fdf1a9deb20e7b2f0506c15b09a00cd281
Ivan Grigor <vanya.grigorevskiy@gmail.com>
2023-10-13 10:13:52 +0300

    sort_form

--- components/Main.jsx
+++ components/Main.jsx
@@ -6,6 +6,27 @@ module.exports = function Main({ user, recipes = [] }) {
   return (
     <Layout>
       <NavBar user={user} />
+      {user && (
+        <>
+          <form className="sort-section flex">
+            <div className="sort-method-container">
+              <label htmlFor="sort-select-label">Тип сортировки: </label>
+              <select name="sort-select">
+                <option value="asc">По возрастанию</option>
+                <option value="desc">По убыванию</option>
+              </select>
+            </div>
+            <div className="sort-method-container">
+              <label htmlFor="sort-type-label">Как сортировать: </label>
+              <select name="sort-type">
+                <option value="ign-count">По количеству ингридиентов</option>
+                <option value="cooking-time">По времени приготовления</option>
+              </select>
+            </div>
+            <button type="submit">Сортировать</button>
+          </form>
+        </>
+      )}
       <div className="recipes-container container flex">
         {user &&
           recipes.map((recipe) => (
