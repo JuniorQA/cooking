6c3028d4225805d7eb332729fbbf5f28916461cd
Ivan Grigor <vanya.grigorevskiy@gmail.com>
2023-10-13 17:10:05 +0300

    fix

--- components/FavoritesPage.jsx
+++ components/FavoritesPage.jsx
@@ -1,17 +1,12 @@
 const React = require('react');
 const Layout = require('./Layout');
-
+const RecipeItem = require('../components/RecipeItem');
 function FavoritesPage({ title, user, recipes }) {
   return (
     <Layout title={title} user={user}>
       <div className="card__container">
         {recipes.map((recipe) => (
-          <div data-id={recipe.id} className="card" style={{ width: '18rem' }}>
-            <img src={recipe.image} className="card-img-top" alt="recipe" />
-            <div className="card-body">
-              <h5 className="card-title">{recipe.name}</h5>
-            </div>
-          </div>
+          <RecipeItem recipe={recipe} />
         ))}
       </div>
     </Layout>
